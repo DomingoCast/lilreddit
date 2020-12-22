3214a7af3e11bd2ce86cbeaa291988379631dcc8
Domingo Castillo <castillomartidomingo@gmail.com>
2020-12-22 08:19:23 +0100

    graphql CRUD

--- lilreddit-server/src/index.ts
+++ lilreddit-server/src/index.ts
@@ -1,16 +1,38 @@
+import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core"
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
-// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up(); 
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
+
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send("casa perro")
+    })
     
-    // const posts = await orm.em.find(Post, {})
-    // console.log(posts)
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({em: orm.em}),
+    })
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000")
+    })
+
+
 }
 
 main().catch((err) => {
